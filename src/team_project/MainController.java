adc811eb2469c53159d59944478234f23eab6007
cyn63 <cyn63@naver.com>
2020-11-05 15:42:39 +0900

    MainController

---
+++ src/team_project/MainController.java
@@ -0,0 +1,27 @@
+package team_project;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainController {
+	MainView _view;
+
+	public MainController() {
+		this._view = AppManager.getInstance().getView();
+
+		this._view.addUserButtonListener(new UserButtonListener());
+
+		// this._campCompView.addUserButtonListener(new UserButtonListener());
+
+	}
+
+	private class UserButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			_view.changeUser();
+
+		}
+
+	}
+}
