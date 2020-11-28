7973a4b7f6bf8e9ede840c767e88f1f09e82f158
seojeong <sjytis14@naver.com>
2020-11-28 22:22:38 +0900

    CampingCar 변수명 수정, Constants로 변수 빼기 완료

---
+++ src/common/Constants.java
@@ -0,0 +1,8 @@
+package common;
+
+public class Constants {
+    public static final String[] CAMPINGCAR_FIELDSTRING = { "carid", "carname", "carno", "seat", "manufacturer", "manu_year", "drivingdistance",
+            "rentcost", "compid", "registdate" };
+    public static final int[] CAMPINGCAR_FIELDLENGTH = { 3, 5, 3, 3, 10, 5, 7, 5, 3, 10 };
+    public static final int CAMPINGCAR_FIELDNUM= 10;
+}
