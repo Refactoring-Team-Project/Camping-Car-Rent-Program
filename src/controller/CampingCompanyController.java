3d67010ffa9b6930fe3f5011c1254b55f5593f0f
min9les <ku2rr9@gmail.com>
2020-12-01 21:50:09 +0900

    campingCompany view model controller 상속

--- src/controller/CampingCompanyController.java
+++ src/controller/CampingCompanyController.java
@@ -11,19 +11,58 @@ import model.CampingCompanyModel;
 import view.CampingCompanyView;
 import view.MainView;
 
-public class CampingCompanyController {
-	MainView _mainView;
-	CampingCompanyView _campCompView;
-	private CampingCompanyModel campCompModel;
+public class CampingCompanyController extends Controller {
 
-	public CampingCompanyController() {
-		this._mainView = AppManager.getInstance().getView();
-		this._campCompView = AppManager.getInstance().getCampingCompanyView();
-		campCompModel = new CampingCompanyModel();
-		this._campCompView.addMouseListener(new CampingCompanyMouseListener());
-		this._campCompView.addButtonListener(new ButtonListener());
+	private CampingCompanyView campingCompanyView;
+	private CampingCompanyModel campingCompanyModel;
 
-		this._mainView.addAdminButtonListener(Constants.CAMPCOMP, new CampingCompanyButtonListener());
+	@Override
+	public void setMainView() {
+		super.setMainView();
+		this._mainView.addAdminButtonListener(Constants.CAMPCOMP, new mainButtonListener());
+	}
+
+	@Override
+	public void initModel() {
+		dataModel = new CampingCompanyModel();
+		campingCompanyModel = (CampingCompanyModel) dataModel;
+	}
+
+	@Override
+	public void initView() {
+		this.thisView = AppManager.getInstance().getCampingCompanyView();
+		campingCompanyView = (CampingCompanyView) this.thisView;
+		this.thisView.addButtonListener(new ButtonListener());
+		this.thisView.addMouseListener(new mainMouseListener());
+	}
+
+	@Override
+	public void setColumnName() {
+		column = new Object[]{"COMPID", "COMPNAME", "ADDRESS", "PHONE", "MANAGER NAME", "MANAGER EMAIL"};
+	}
+
+	@Override
+	public void setModelColumn(String column, String value) {
+		switch (column) {
+			case "compid":
+				campingCompanyModel.setCompid(Integer.parseInt(value));
+				break;
+			case "compname":
+				campingCompanyModel.setCompname(value);
+				break;
+			case "address":
+				campingCompanyModel.setAddress(value);
+				break;
+			case "phone":
+				campingCompanyModel.setPhone(value);
+				break;
+			case "manager_name":
+				campingCompanyModel.setManager_name(value);
+				break;
+			case "manager_email":
+				campingCompanyModel.setManager_email(value);
+				break;
+		}
 	}
 
 	private class ButtonListener implements ActionListener {
@@ -32,66 +71,24 @@ public class CampingCompanyController {
 		public void actionPerformed(ActionEvent e) {
 
 			try {
-				if (e.getSource() == _campCompView.btnInput) {
-					if (_campCompView.campingCompanyInputField[0].getText().length() > 0) {
-						campCompModel.setCompid(Integer.parseInt(_campCompView.campingCompanyInputField[0].getText()));
-					}
-					if (_campCompView.campingCompanyInputField[1].getText().length() > 0) {
-						campCompModel.setCompname(_campCompView.campingCompanyInputField[1].getText());
-					}
-					if (_campCompView.campingCompanyInputField[2].getText().length() > 0) {
-						campCompModel.setAddress(_campCompView.campingCompanyInputField[2].getText());
-					}
-					if (_campCompView.campingCompanyInputField[3].getText().length() > 0) {
-						campCompModel.setPhone(_campCompView.campingCompanyInputField[3].getText());
-					}
-					if (_campCompView.campingCompanyInputField[4].getText().length() > 0) {
-						campCompModel.setManager_name(_campCompView.campingCompanyInputField[4].getText());
-					}
-					if (_campCompView.campingCompanyInputField[5].getText().length() > 0) {
-						campCompModel.setManager_email(_campCompView.campingCompanyInputField[5].getText());
-					}
-	
-					campCompModel.insert(_mainView.getConn());
-	
-					
-				} else if (e.getSource() == _campCompView.btnDelete) {
+				if (e.getSource() == campingCompanyView.btnInput)
+				{
+					setModel();
+					campingCompanyModel.insert(_mainView.getConn());
+					thisView.fieldReset();
+				} else if (e.getSource() == campingCompanyView.btnDelete) {
 					if (_mainView.getCurRow() != -1) {
-						campCompModel.delete(_mainView.getConn(), _campCompView.campingCompanyDBResult.getModel().getValueAt(_mainView.getCurRow(), 0));
-						_campCompView.fieldReset();
+						campingCompanyModel.delete(_mainView.getConn(), thisView.DBResult.getModel().getValueAt(_mainView.getCurRow(), 0));
+						thisView.fieldReset();
 					} else
 						JOptionPane.showMessageDialog(null, "삭제할 데이터를 선택해 주세요.");
-	
-				} else if (e.getSource() == _campCompView.btnUpdate) {
+				} else if (e.getSource() == campingCompanyView.btnUpdate) {
 					if (_mainView.getCurRow() != -1) {
-						if (_campCompView.campingCompanyInputField[0].getText().length() > 0) {
-							campCompModel.setCompid(Integer.parseInt(_campCompView.campingCompanyInputField[0].getText()));
-						} else
-							throw new NullPointerException();
-					
-						if (_campCompView.campingCompanyInputField[1].getText().length() > 0) {
-							campCompModel.setCompname(_campCompView.campingCompanyInputField[1].getText());
-						}
-						if (_campCompView.campingCompanyInputField[2].getText().length() > 0) {
-							campCompModel.setAddress(_campCompView.campingCompanyInputField[2].getText());
-						}
-						if (_campCompView.campingCompanyInputField[3].getText().length() > 0) {
-							campCompModel.setPhone(_campCompView.campingCompanyInputField[3].getText());
-						}
-						if (_campCompView.campingCompanyInputField[4].getText().length() > 0) {
-							campCompModel.setManager_name(_campCompView.campingCompanyInputField[4].getText());
-						}
-						if (_campCompView.campingCompanyInputField[5].getText().length() > 0) {
-							campCompModel.setManager_email(_campCompView.campingCompanyInputField[5].getText());
-						}
-						campCompModel.update(_mainView.getConn(),
-								_campCompView.campingCompanyDBResult.getModel().getValueAt(_mainView.getCurRow(), 0));
-						_campCompView.fieldReset();
+						setModel();
+						campingCompanyModel.update(_mainView.getConn(), thisView.DBResult.getModel().getValueAt(_mainView.getCurRow(), 0));
 					} else
 						JOptionPane.showMessageDialog(null, "변경할 데이터를 선택해 주세요.");
 				}
-		
-				_campCompView.fieldReset();
 		} catch (NullPointerException e2) {
 			JOptionPane.showMessageDialog(null, "null");
 	
@@ -99,33 +96,5 @@ public class CampingCompanyController {
 		}
 
 	}
-	
-	private class CampingCompanyMouseListener extends MouseAdapter {
-		public void mouseClicked(MouseEvent e) {
-			_mainView.setCurRow(_campCompView.campingCompanyDBResult.getSelectedRow());
-			_mainView.setCurCol(_campCompView.campingCompanyDBResult.getSelectedColumn());
-		}
-	}
-
-	private class CampingCompanyButtonListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			_mainView.changePanel(_campCompView);
-			_mainView.setCurRow(-1);
-			_mainView.setCurCol(-1);
-
-			ArrayList<Object[]> arr = campCompModel.select(_mainView.getConn());
-			Object column[] = {"COMPID", "COMPNAME", "ADDRESS", "PHONE", "MANAGER NAME", "MANAGER EMAIL"};
-			arr.add(0, column);
-			_campCompView.campingCompanyDefaultTable.setDataVector(null, arr.get(0));
-			for (int i = 1; i < arr.size(); i++) {
-				_campCompView.campingCompanyDefaultTable.addRow(arr.get(i));
-			}
-			System.out.println("campcomp");
-			_mainView.revalidate();
-			_mainView.repaint();
-		}
-
-	}
 }
+
