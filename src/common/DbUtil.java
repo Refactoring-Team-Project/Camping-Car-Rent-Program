c7bfd60275c26fab25aab2e2e2ede4a28d288713
min9les <ku2rr9@gmail.com>
2020-12-03 04:31:01 +0900

    dbUtil 변수명 마저 수정

--- src/common/DbUtil.java
+++ src/common/DbUtil.java
@@ -3,7 +3,9 @@ package common;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
@@ -11,16 +13,17 @@ import javax.swing.JOptionPane;
 public class DbUtil {
 
 	/******메서드를 메서드 객체로 전환******/
-	public static ArrayList<Object[]> getRows(Connection conn, String sqlQuery) {
+	public static ArrayList<Object[]> getRows(Connection connection, String sqlQuery) {
+
 		ArrayList<Object[]> rowList = new ArrayList<>();
 		try {
-			ResultSet rs = conn.createStatement().executeQuery(sqlQuery);
-			int columnCount = rs.getMetaData().getColumnCount();
+			ResultSet resultSet = connection.createStatement().executeQuery(sqlQuery);
+			int columnCount = resultSet.getMetaData().getColumnCount();
 
-			while (rs.next()) {
+			while (resultSet.next()) {
 				Object[] data = new Object[columnCount];
 				for (int i = 0; i < columnCount; i++) {
-					data[i] = rs.getString(i + 1);
+					data[i] = resultSet.getString(i + 1);
 				}
 				;
 				rowList.add(data);
@@ -31,9 +34,9 @@ public class DbUtil {
 		return rowList;
 	}
 
-	public static void execute(Connection conn, String sqlQuery, String[] types, Object[] values) {
+	public static void execute(Connection connection, String sqlQuery, String[] types, Object[] values) {
 		try {
-			PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
+			PreparedStatement pstmt = connection.prepareStatement(sqlQuery);
 			if (types != null)
 				for (int i = 0; i < types.length; i++) {
 					switch (types[i]) {
